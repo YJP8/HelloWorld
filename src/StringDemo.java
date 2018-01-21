7a71965d807c75e5f67441c409b9dc6c9c8e46ea
于景鹏 <yjp__hope@163.com>
2018-01-21 22:07:51 +0800

    字符串拼接,属性

---
+++ src/StringDemo.java
@@ -0,0 +1,11 @@
+
+public class StringDemo{
+    public static void main(String args[]) {
+        String site = "www.runoob.com";
+        int len = site.length();
+        System.out.println( "菜鸟教程网址长度 : " + len );
+        String string1 = "菜鸟教程网址：";
+        System.out.println("1、" + string1 + "www.runoob.com");
+    }
+}
+
